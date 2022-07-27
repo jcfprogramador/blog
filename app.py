a7ef663933e938f238b5278db3d7d0541f5e7944
Júlio C. Franco <jcfprogramador@gmail.com>
2022-07-26 22:36:05 -0300

    Novo template herdando de base.html e criado style.css - Fim Aula 3

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
 
+posts = [
+    {
+        "title": "Meu primeiro post",
+        "body": "Eu escrevi este post para testar a pagina no flask",
+        "description": "teste post flask",
+        "author": "Julio C. Franco",
+        "created": datetime(2002,7,25)
+    },
+    {
+        "title": "O meu Segundo Post",
+        "body": "Teste do Segundo Post",
+        "author": "Julio C. Franco",
+        "created": datetime(2022,7,26)
+    },
+]
+
 @app.route("/")
-@app.route("/hello")
-
-
-def hello():
-    return "Ola"
-
-@app.route("/meucontato")
-def meuContato():
-    return render_template("index.html")
-
+def index(debug=True):
+    return render_template("index.html", posts=posts)
+    
 app.run(debug=True)
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
