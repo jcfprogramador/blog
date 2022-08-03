5d4826f3f97e345f8b5f1785634359c7ed3d4e3e
Júlio C. Franco <jcfprogramador@gmail.com>
2022-08-03 10:29:29 -0300

    Formatações no codigo

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.exc import IntegrityError
 import os
 
-## https://blog-jcf-ocean.herokuapp.com/ | https://git.heroku.com/blog-jcf-ocean.git
+## https://blogjcf.herokuapp.com/ | https://git.heroku.com/blogjcf.git
 
 
 app = Flask("hello")
@@ -38,12 +38,12 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 
 class Post(db.Model):
-    __tablename__ = 'posts'
+    __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(70), nullable=False)
     body = db.Column(db.String(500))
     created = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
@@ -51,7 +51,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
     email = db.Column(db.String(64), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
-    posts = db.relationship('Post', backref='author')
+    posts = db.relationship("Post", backref="author")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -66,7 +66,7 @@ def load_user(id):
 
 db.create_all()
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
     posts = Post.query.order_by(-Post.created).all()
     return render_template("index.html", posts=posts)
@@ -74,11 +74,11 @@ def index():
 @app.route('/register', methods=["GET","POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     if request.method == "POST":
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
         try:
             new_user = User(username=username, email=email)
             new_user.set_password(password)
@@ -87,40 +87,40 @@ def register():
         except IntegrityError:
             flash("Username or E-mail already exists!")
         else:
-            return redirect(url_for('login'))
-    return render_template('register.html')
+            return redirect(url_for("login"))
+    return render_template("register.html")
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     if request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form["username"]
+        password = request.form["password"]
         user = User.query.filter_by(username=username).first()
         if user is None or not user.check_password(password):
             flash("Incorrect Username or Password")
-            return redirect(url_for('login'))
+            return redirect(url_for("login"))
         login_user(user)
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     return render_template("login.html")
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 @app.route('/create', methods=["GET", "POST"])
 @login_required
 def create():
     if request.method == "POST":
-        title = request.form['title']
-        body = request.form['body']
+        title = request.form["title"]
+        body = request.form["body"]
         try:
             post = Post(title=title, body=body, author=current_user)
             db.session.add(post)
             db.session.commit()
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
         except IntegrityError:
             flash("Error on create Post, try again later")
-    return render_template('create.html')
+    return render_template("create.html")
